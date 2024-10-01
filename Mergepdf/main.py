3b04f346c06dc6663dcbbb8df2d0dbd2509261e7
code with ovesh <oveshaziz123@gmail.com>
2024-10-01 12:53:29 -0700

    second commit

--- Mergepdf/main.py
+++ Mergepdf/main.py
@@ -1,1 +1,14 @@
-print("hello world")
+import PyPDF2
+
+# Merging PDF files
+pdf_files = ['pdf1.pdf', 'pdf2.pdf',]  # aapke PDF files ke naam yahan
+merger = PyPDF2.PdfWriter()
+
+for pdf in pdf_files:
+    merger.append(pdf)
+
+# Merged file ka naam
+merger.write('merged.pdf')
+merger.close()
+
+print("PDF files merged successfully!")
