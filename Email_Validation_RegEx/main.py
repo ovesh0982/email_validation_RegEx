2877027714e1d85ecaca29c041d7263236f56a84
code with ovesh <oveshaziz123@gmail.com>
2024-10-01 11:41:25 -0700

    Second Commit

--- Email_Validation_RegEx/main.py
+++ Email_Validation_RegEx/main.py
@@ -1,1 +1,10 @@
-print("hello world")
+print("Email_validation_RegEx")
+# first_condition = a-z  oveshaziz123@gmail.com
+# first_condition = 0-9
+# first_condition = . _ time 1
+# first_condition = @ time 1
+# first_condition = . 2,3 position pe hona chaiye
+
+
+import re
+email_condition = "^[a-z]+[\._]"
